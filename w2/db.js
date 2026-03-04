d7b9a5cf8d6c7ed564423f1cc650fb2986c53085
DriscollDev <cdriscolldev@gmail.com>
2026-03-04 17:04:18 -0500

    yuh

--- w2/db.js
+++
@@ -1,6 +0,0 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const dotenv = require('dotenv');
-// Load environment variables from .env file
-dotenv.config();
-
-run().catch(console.dir);
